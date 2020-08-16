7788edb953090eb3f4dab0c58c8e730f295884ff
dinhhanpw <55496521+dinhhanpw@users.noreply.github.com>
2020-08-16 15:34:15 +0700

    Implement form validation

--- Library_Management_Project/Library_Management_Project/Helper/Validator/BookValidator.cs
+++ Library_Management_Project/Library_Management_Project/Helper/Validator/BookValidator.cs
@@ -1,11 +1,99 @@
 ﻿using FluentValidation;
 using Library_Management_Project.Model;
+using System;
 
 namespace Library_Management_Project.Helper.Validator
 {
     public class BookValidator : AbstractValidator<Sach>
     {
-        
-        
+
+        private int MinLength { get; set; } = 5;
+
+        public BookValidator()
+        {
+            // đặt chế độ dừng khi phát hiện lỗi
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            // đặt các điều kiện cho tên sách
+            RuleSet("Ten", () =>
+            {
+                RuleFor(book => book.Ten)
+                // tên sách là thuộc tính bắt buộc
+                .NotNull().WithMessage($"Tên sách {ValidationHelper.NotEmpty_ErrMessage}")
+                // tên sách không được trống
+                .NotEmpty().WithMessage($"Tên sách {ValidationHelper.NotEmpty_ErrMessage}")
+                // đô dài tối thiểu của tên
+                .MinimumLength(MinLength).WithMessage($"Tên sách {ValidationHelper.MustLeast_ErrMessage} {MinLength} kí tự");
+            });
+
+            // đặt điều kiện cho ngày nhập sách
+            RuleSet("NgayNhap", () =>
+            {
+                RuleFor(book => book.NgayNhap)
+                // ngày nhập là thuộc tính bắt buộc
+                .NotNull().WithMessage($"Ngày nhập sách {ValidationHelper.NotEmpty_ErrMessage}")
+                // khoảng ngày nhập hợp lệ
+                .Must((book, val) =>
+                {
+                    // trong năm xuất bản đến ngày hiện tại
+                    return val.Value.Year >= book.NamXB && val <= DateTime.Now;
+
+                }).WithMessage($"Ngày nhập {ValidationHelper.InValid_ErrMessage}");
+            });
+
+            // đặt các điều kiện cho thuộc tính tác giả
+            RuleSet("TacGia", () =>
+            {
+                RuleFor(book => book.TacGia)
+                // tên tác giả là thuộc bắt buộc
+                .NotNull().WithMessage($"Tên tác giả {ValidationHelper.NotEmpty_ErrMessage}")
+                // tên tác giả không được trống
+                .NotEmpty().WithMessage($"Tên tác giả {ValidationHelper.NotEmpty_ErrMessage}")
+                // độ dài tối thiểu của tên tác giả
+                .MinimumLength(MinLength).WithMessage($"Tên tác giả {ValidationHelper.MustLeast_ErrMessage} {MinLength} kí tự");
+            });
+
+            // đặt điều kiện cho thuộc tính nhà xuất bản
+            RuleSet("NhaXB", () =>
+            {
+                RuleFor(book => book.NhaXB)
+                // nhà xuất bản là thuộc tính bắt buộc
+                .NotEmpty().WithMessage($"Nhà xuất bản {ValidationHelper.NotEmpty_ErrMessage}")
+                // nhà xuất bản không được trống
+                .NotEmpty().WithMessage($"Nhà xuất bản {ValidationHelper.NotEmpty_ErrMessage}")
+                // độ dài tối thiểu của tên nhà xuất bản
+                .MinimumLength(MinLength).WithMessage($"Tên nhà xuất bản {ValidationHelper.MustLeast_ErrMessage} {MinLength} kí tự");
+            });
+
+            // đặt điều kiện cho thuộc tính năm xuất bản
+            RuleSet("NamXB", () =>
+            {
+                RuleFor(book => book.NamXB)
+                // năm xuất bản là thuộc tính bắt buộc
+                .NotNull().WithMessage($"Năm xuất bản {ValidationHelper.NotEmpty_ErrMessage}")
+                // năm xuất bản phải nằm trong khoảng năm nhất định theo quy định
+                .GreaterThanOrEqualTo(DateTime.Now.Year - 8).WithMessage($"Năm xuất bản {ValidationHelper.InValid_ErrMessage}");
+            });
+
+            // đặt điều kiện cho thuộc tính số lượng sách
+            RuleSet("SoLuong", () =>
+            {
+                RuleFor(book => book.SoLuong)
+                // số lượng sách là thuộc tính bắt buộc
+                .NotNull().WithMessage($"Số lượng sách {ValidationHelper.NotEmpty_ErrMessage}")
+                // số lượng sách phải ít nhất là 1
+                .GreaterThan(0).WithMessage($"Số lượng sách {ValidationHelper.InValid_ErrMessage}");
+            });
+
+            // đặt điều kiện cho thuộc tính số giá sách
+            RuleSet("Gia", () =>
+            {
+                RuleFor(book => book.Gia)
+                // giá sách là thuộc tính bắt buộc
+                .NotNull().WithMessage($"Giá sách {ValidationHelper.NotEmpty_ErrMessage}")
+                // giá sách phải lớn hớn 500 đồng
+                .GreaterThan(500).WithMessage($"Giá sách {ValidationHelper.InValid_ErrMessage}");
+            });
+        }
     }
 }
--- Library_Management_Project/Library_Management_Project/Model/Sach.cs
+++ Library_Management_Project/Library_Management_Project/Model/Sach.cs
@@ -13,7 +13,7 @@ namespace Library_Management_Project.Model
     using Library_Management_Project.Helper.Validator;
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Sach : ValidatableBase<Sach>
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,23 +21,65 @@ namespace Library_Management_Project.Model
         {
             this.CTPhieuMuons = new HashSet<CTPhieuMuon>();
         }
-    
+
         static Sach()
         {
             validator = new BookValidator();
         }
 
+        private string ten;
+        private int? idLoai;
+        private string tacGia;
+        private int? namXB;
+        private string nhaXB;
+        private DateTime? ngayNhap;
+        private int? soLuong;
+        private int? gia;
+
+
         public int Id { get; set; }
-        public string Ten { get; set; }
-        public Nullable<int> IdLoai { get; set; }
-        public string TacGia { get; set; }
-        public Nullable<int> NamXB { get; set; }
-        public string NhaXB { get; set; }
-        public Nullable<DateTime> NgayNhap { get; set; }
-        public Nullable<int> SoLuong { get; set; }
-        public Nullable<int> Gia { get; set; }
+        public string Ten
+        {
+            get { return ten; }
+            set { SetValidatableProperty(ref ten, value, this); }
+        }
+        public Nullable<int> IdLoai
+        {
+            get { return idLoai; }
+            set { SetBindableProperty(ref idLoai, value); }
+        }
+        public string TacGia
+        {
+            get { return tacGia; }
+            set { SetValidatableProperty(ref tacGia, value, this); }
+        }
+        public Nullable<int> NamXB
+        {
+            get { return namXB; }
+            set { SetValidatableProperty(ref namXB, value, this); }
+        }
+        public string NhaXB
+        {
+            get { return nhaXB; }
+            set { SetValidatableProperty(ref nhaXB, value, this); }
+        }
+        public Nullable<DateTime> NgayNhap
+        {
+            get { return ngayNhap; }
+            set { SetValidatableProperty(ref ngayNhap, value, this); }
+        }
+        public Nullable<int> SoLuong
+        {
+            get { return soLuong; }
+            set { SetValidatableProperty(ref soLuong, value, this); }
+        }
+        public Nullable<int> Gia
+        {
+            get { return gia; }
+            set { SetValidatableProperty(ref gia, value, this); }
+        }
         public Nullable<bool> BiAn { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CTPhieuMuon> CTPhieuMuons { get; set; }
         public virtual LoaiSach LoaiSach { get; set; }
