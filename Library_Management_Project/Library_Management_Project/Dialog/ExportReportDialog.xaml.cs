2fa0c57d89aa8d5075c80013a870d0416ef09d03
dinhhanpw <55496521+dinhhanpw@users.noreply.github.com>
2020-08-16 10:58:43 +0700

    Implement exporting reader

---
+++ Library_Management_Project/Library_Management_Project/Dialog/ExportReportDialog.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Library_Management_Project.Helper;
+using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Library_Management_Project.Dialog
+{
+    /// <summary>
+    /// Interaction logic for ExportReportDialog.xaml
+    /// </summary>
+    public partial class ExportReportDialog : Window
+    {
+        public ExportReportDialog()
+        {
+            InitializeComponent();
+            BrowserCommand = new RelayCommand<Object>(obj => true, OnBrowser);
+            AcceptCommand = new RelayCommand<Object>(CanAccept, OnAccept);
+            this.DataContext = this;
+        }
+        public string FileName { get; set; }
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+
+        public ICommand BrowserCommand { get; set; }
+        public ICommand AcceptCommand { get; set; }
+
+        /// <summary>
+        /// chọn đường dẫn để lưu tập tin báo cáo
+        /// </summary>
+        /// <param name="obj"></param>
+        private void OnBrowser(object obj)
+        {
+            // tạo và hiển thị hộp thoại để chọn tập tin lưu
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Excel file (*.xlsx)|*.xlsx";
+            DialogResult result = dialog.ShowDialog();
+
+            // lấy đường dẫn tâp tin
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                FileName = dialog.FileName;
+                fileNameTxtBlock.Text = FileName;
+            }
+        }
+
+        /// <summary>
+        /// kiểm tra sự hơp lệ của của thông tin để cho phép chấp nhận xuất báo cáo
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private bool CanAccept(object obj)
+        {
+            return !String.IsNullOrWhiteSpace(FileName)
+                && From != null
+                && To != null
+                && From < To;
+        }
+
+        /// <summary>
+        /// trả về chấp nhận xuất và đóng hộp thoại
+        /// </summary>
+        /// <param name="obj"></param>
+        private void OnAccept(object obj)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+
+    }
+}
--- Library_Management_Project/Library_Management_Project/ViewModel/ReaderViewModel.cs
+++ Library_Management_Project/Library_Management_Project/ViewModel/ReaderViewModel.cs
@@ -1,7 +1,12 @@
-﻿using Library_Management_Project.Helper;
+﻿using Aspose.Cells;
+using Library_Management_Project.Dialog;
+using Library_Management_Project.Helper;
 using Library_Management_Project.Model;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using Telerik.Windows.Controls;
@@ -37,6 +42,7 @@ namespace Library_Management_Project.ViewModel
         public ICommand AddCommand { get; set; }
         public ICommand EditCommand { get; set; }
         public ICommand DeleteCommand { get; set; }
+        public ICommand ExportCommand { get; set; }
 
         //public ICommand Add_EditOnTableDataCommand { get; set; }
 
@@ -52,8 +58,90 @@ namespace Library_Management_Project.ViewModel
             AddCommand = new RelayCommand<DocGia>(CanAdd, OnAdd);
             EditCommand = new RelayCommand<DocGia>(CanEdit, OnEdit);
             DeleteCommand = new RelayCommand<DocGia>(CanDelete, OnDelete);
+            ExportCommand = new RelayCommand<Object>(obj => true, OnExport);
             //Add_EditOnTableDataCommand = new RelayCommand<GridViewRowEditEndedEventArgs>(argument => argument != null, OnAdd_Edit);
             //Holder.LoaiDocGia = new LoaiDocGia();
+        }
+
+        /// <summary>
+        /// xuất thông tin của đọc giả đến tập tin excel
+        /// </summary>
+        /// <param name="obj"></param>
+        private void OnExport(object obj)
+        {
+            ExportReportDialog dialog = new ExportReportDialog();
+
+            // mở hộp thoại để đặt các thông tin cần thiết
+            if(dialog.ShowDialog() == true)
+            {
+                Workbook workbook = new Workbook();
+                Worksheet worksheet = workbook.Worksheets[0];
+                // lấy danh sách đọc giả theo tiêu chí đã chọn
+                List<DocGia> readerList = 
+                    new List<DocGia>(DataProvider.Instance.DataBase.DocGias.Where(reader => reader.NgayLap >= dialog.From && reader.NgayLap<=dialog.To));
+                int row = 3;
+
+                SetHeader(worksheet);
+                
+                // lưu thông tin đọc giả vào tập tin excel
+                foreach (DocGia reader in readerList)
+                {
+                    SetValueOnRowWorksheet(worksheet, reader, row++);
+                }
+
+                workbook.Save(dialog.FileName, SaveFormat.Xlsx);
+            }
+        }
+
+        /// <summary>
+        /// đặt các tiêu đề cột trong tập tin excel
+        /// </summary>
+        /// <param name="worksheet"></param>
+        /// <param name="row"></param>
+        private void SetHeader(Worksheet worksheet, int row = 2)
+        {
+            char startCol = 'A';
+
+            Cell cell = worksheet.Cells[$"{startCol}{row}"];
+            cell.PutValue("STT");
+            cell = worksheet.Cells[$"{(char)(startCol + 1)}{row}"];
+            cell.PutValue("Họ Và Tên");
+            cell = worksheet.Cells[$"{(char)(startCol + 2)}{row}"];
+            cell.PutValue("Loại Đọc Giả");
+            cell = worksheet.Cells[$"{(char)(startCol + 3)}{row}"];
+            cell.PutValue("Ngày Sinh");
+            cell = worksheet.Cells[$"{(char)(startCol + 4)}{row}"];
+            cell.PutValue("Địa Chỉ");
+            cell = worksheet.Cells[$"{(char)(startCol + 5)}{row}"];
+            cell.PutValue("Email");
+            cell = worksheet.Cells[$"{(char)(startCol + 6)}{row}"];
+            cell.PutValue("Ngày Lập Thẻ");
+        }
+
+        /// <summary>
+        /// lưu một đối tượng đọc giả vào một hàng của tập tin excel
+        /// </summary>
+        /// <param name="worksheet"></param>
+        /// <param name="reader"></param>
+        /// <param name="row"></param>
+        /// <param name="startCol"></param>
+        private void SetValueOnRowWorksheet(Worksheet worksheet, DocGia reader, int row, char startCol = 'A')
+        {
+
+            Cell cell = worksheet.Cells[$"{(char)(startCol)}{row}"];
+            cell.PutValue(row - 2);
+            cell = worksheet.Cells[$"{(char)(startCol + 1)}{row}"];
+            cell.PutValue(reader.Ten);
+            cell = worksheet.Cells[$"{(char)(startCol + 2)}{row}"];
+            cell.PutValue(reader.LoaiDocGia.Ten);
+            cell = worksheet.Cells[$"{(char)(startCol + 3)}{row}"];
+            cell.PutValue(reader.NgaySinh.Value.ToString("MM/dd/yyyy"));
+            cell = worksheet.Cells[$"{(char)(startCol + 4)}{row}"];
+            cell.PutValue(reader.DiaChi);
+            cell = worksheet.Cells[$"{(char)(startCol + 5)}{row}"];
+            cell.PutValue(reader.Email);
+            cell = worksheet.Cells[$"{(char)(startCol + 6)}{row}"];
+            cell.PutValue(reader.NgayLap.Value.ToString("MM/dd/yyyy"));
         }
 
         /// <summary>
