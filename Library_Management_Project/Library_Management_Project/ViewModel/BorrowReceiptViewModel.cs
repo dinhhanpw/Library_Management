c01550fd5a5d80ec7cedd1771ac07db46cff246b
dinhhanpw <55496521+dinhhanpw@users.noreply.github.com>
2020-08-16 20:27:52 +0700

    Design UI

---
+++ Library_Management_Project/Library_Management_Project/ViewModel/BorrowReceiptViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Library_Management_Project.Helper;
+using Library_Management_Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Library_Management_Project.ViewModel
+{
+    public class BorrowReceiptViewModel
+    {
+
+        private Sach selectedBook;
+
+        public Sach SelectedBook
+        {
+            get { return selectedBook; }
+            set
+            {
+                selectedBook = value;
+                if (value == null) return;
+
+                Holder.Ten = SelectedBook.Ten;
+                Holder.IdLoai = SelectedBook.IdLoai;
+                Holder.TacGia = SelectedBook.TacGia;
+                Holder.NamXB = SelectedBook.NamXB;
+                Holder.NhaXB = SelectedBook.NhaXB;
+                Holder.NgayNhap = SelectedBook.NgayNhap;
+                Holder.SoLuong = SelectedBook.SoLuong;
+                Holder.Gia = SelectedBook.Gia;
+            }
+        }
+        public Sach Holder { get; set; }
+        public ObservableCollection<PhieuMuon> BorrowReceipts { get; set; }
+        public ObservableCollection<DocGia> Readers { get; set; }
+        public ObservableCollection<Sach> Books { get; set; }
+
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand ExportCommand { get; set; }
+
+
+
+
+        public BorrowReceiptViewModel()
+        {
+            Readers = DataProvider.Instance.Readers;
+            Books = DataProvider.Instance.Books;
+            BorrowReceipts = DataProvider.Instance.BorrowReceipts;
+            Holder = new Sach();
+            Holder.IdLoai = BorrowReceipts[0].Id;
+
+        }
+    }
+}
--- Library_Management_Project/Library_Management_Project/ViewModel/MainViewModel.cs
+++ Library_Management_Project/Library_Management_Project/ViewModel/MainViewModel.cs
@@ -11,7 +11,7 @@ namespace Library_Management_Project.ViewModel
         {
             Items.Add(new NavigationItem() { Title = "Quản Lý Đọc Giả", IconGlyph = "&#xe801", ViewContent = new ReaderViewModel() });
             Items.Add(new NavigationItem() { Title = "Quản Lý Sách", IconGlyph = "&#xe651", ViewContent = new BookViewModel() });
-            Items.Add(new NavigationItem() { Title = "Quản Lý Mượn Sách", IconGlyph = "&#xe131", ViewContent = null});
+            Items.Add(new NavigationItem() { Title = "Quản Lý Mượn Sách", IconGlyph = "&#xe131", ViewContent = new BorrowReceiptViewModel()});
             Items.Add(new NavigationItem() { Title = "Quản Lý Trả Sách", IconGlyph = "&#xe130", ViewContent = null });
             Items.Add(new NavigationItem() { Title = "Quy Định", IconGlyph = "&#xe401", ViewContent = null });
         }
